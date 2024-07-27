ebdad832fee07d43da7c18fb22d51da8402d846b
nimit sodhani <nimitsodhani1@gmail.com>
2024-07-28 05:08:41 +0530

    div 3 contest

---
+++ div4/farmer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std  ;
+int main ()
+{
+	int t  ;
+	cin  >> t  ;
+	while (t--)
+	{
+		int n  ;
+		cin  >> n    ;
+		if  ( n % 4 ==  0 )
+			cout << n / 4 << endl  ;
+		else cout  << (n / 4) + 1 << endl  ;
+
+	}
+	return   0;
+}
