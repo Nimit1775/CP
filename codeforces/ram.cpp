e6ac57e6bc41cc3f529b979d4a3c8938b4496137
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-02 20:28:29 +0530

    practice

---
+++ codeforces/ram.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std    ;
+int main ()
+{
+	int t   ;
+	cin >> t  ;
+	while (t--)
+	{
+		int n  , k   ;
+		cin  >> n  >> k ;
+		int  tt = (n - 1) * k + 1   ;
+		cout << tt << endl  ;
+
+	}
+	return  0  ;
+}
