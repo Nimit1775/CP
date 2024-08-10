5d91ded97b72cd6202575b316217e952bbdf3635
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-10 23:51:00 +0530

    tle-eliminators

---
+++ TLE/welcome.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std   ;
+int main ()
+{
+	int A , B  ;
+	cin >> A >> B  ;
+	if  ( A >= B) {
+		cout << "Yes" ;
+	}
+	else {
+		cout << "No" ;
+	}
+	return 0  ;
+}
