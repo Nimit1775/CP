d33f1b2f7048e1a858fbfc12f6ec66c8284779ff
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-13 20:51:52 +0530

    practice

---
+++ PRACTICE/DTcount.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        string x, s;
+        cin >> x >> s;
+
+        string current_x = x;
+        int operations = 0 ;
+
+        // Keep concatenating until current_x is at least as long as s
+        while (current_x.length() < m) {
+            current_x += x;
+            operations++;
+        }
+
+        // Check if s is already a substring of current_x
+        if (current_x.find(s) != string::npos) {
+            cout << operations << endl;
+        } else {
+            current_x += x;
+            operations++;
+            if (current_x.find(s) != string::npos) {
+                cout << operations << endl;
+            } else {
+                cout << -1 << endl;
+            }
+        }
+    }
+
+    return 0;
+}
