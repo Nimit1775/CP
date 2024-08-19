1815b195d2da677439fdc6414a1e587b4c5aa26e
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-19 18:20:25 +0530

    practice

---
+++ PRACTICE/again25.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std  ;
+
+void solve  ()
+{
+	long long n  ;
+	cin >>  n ;
+	long long  last ;
+	if (n >= 2) last = 25  ;
+	else last = 5 ;
+	cout << last << endl  ;
+}
+int main ()
+{
+
+	solve()  ;
+	return 0 ;
+}
