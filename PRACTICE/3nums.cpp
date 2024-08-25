17c622795797e470a91f0ebde433f71863d4f345
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-25 15:37:18 +0530

    tle 900

---
+++ PRACTICE/3nums.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve ()
+{
+	int k  , s ;
+	cin  >>  k >> s  ;
+
+}
+int main  ()
+{
+	solve()  ;
+	return  0  ;
+}
--- PRACTICE/Sleep.cpp
+++ PRACTICE/Sleep.cpp
@@ -3,8 +3,27 @@ using namespace std ;
 
 void solve ()
 {
-	cout << " kal karenge bhai " ;
+	int n , H , M ;
+	cin >> n >> H >> M ;
+	int bed_time = H * 60 + M  ;
+	int min_sleep_time = 1440  ;
+	for  ( int i = 0  ; i < n ; i++)
+	{
+		int h , m  ;
+		cin  >> h  >> m   ;
+		int alarm_time = h * 60 +  m;
 
+		int sleep_time = alarm_time - bed_time ;
+		if (sleep_time < 0) {
+			sleep_time += 1440;
+		}
+
+		min_sleep_time = min(sleep_time , min_sleep_time)  ;
+
+	}
+	int hours = min_sleep_time / 60  ;
+	int mins =  min_sleep_time % 60  ;
+	cout  << hours << " " << mins << endl ;
 }
 int main  ()
 {
@@ -13,6 +32,7 @@ int main  ()
 	while (t--)
 	{
 		solve()  ;
-		return 0  ;
+
 	}
+	return 0  ;
 }
